07ff2face7634c0da651db8c1b41142270b0e424
Partha Sarathi Gouda <33528240+parthasgouda@users.noreply.github.com>
2017-11-18 23:42:12 -0700

    Add files via upload

--- my_camera/include/camera_node.h
+++ my_camera/include/camera_node.h
@@ -2,6 +2,7 @@
 #define __CAMERA_NODE_H__
 
 #include <iostream>
+#include <stdlib.h>
 #include <ros/ros.h>
 #include <cv_bridge/cv_bridge.h>
 #include <opencv2/opencv.hpp>
--- my_camera/src/camera_node.cpp
+++ my_camera/src/camera_node.cpp
@@ -4,17 +4,33 @@ int main(int argc, char** argv){
 
 ros::init(argc,argv,"my_camera");
 
-ros::NodeHandle n;
+ros::NodeHandle n("~");
+
+std::string device, device1;
+
+if(!n.hasParam("device_name")){
+	n.setParam("device_name","/dev/video0");
+}
+
+n.getParam("device_name", device);
 
 ros::Publisher camera = n.advertise<sensor_msgs::Image>("partha_topic",1000);
 
 ros::Rate loop_rate(30);
 
-cv::VideoCapture cap(1);
+cv::VideoCapture cap;
+cap.open(device);
 cv::Mat cv_img=cv::Mat(480,640,CV_8UC3);
 int c=0;
 while(ros::ok()){
 
+n.getParam("device_name", device1);
+ROS_INFO("%s\n",device.c_str());
+	if(device.compare(device1) !=0){
+		cap.release();
+		cap.open(device1);
+		device=device1;
+}
 sensor_msgs::Image img;
 cap>>cv_img;
 
